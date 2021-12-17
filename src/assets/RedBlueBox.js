a8982a699781b42606577f85e0e436250a2c203b
jayouimet <jayouimet@hotmail.com>
2021-12-17 13:37:59 -0500

    weird change

--- src/assets/RedBlueBox.js
+++ src/assets/RedBlueBox.js
@@ -2,9 +2,9 @@ import React from 'react';
 import RedBox from './RedBox';
 import BlueBox from './BlueBox';
 
-const RedBlueBox = ({position, onclick, ...props}) => {
+const RedBlueBox = ({position, onClick, ...props}) => {
     return(
-        <a-entity position={position} onClick={onclick}>
+        <a-entity position={position} onClick={onClick}>
             <RedBox {...props}/>
             <BlueBox {...props}/>
         </a-entity>
