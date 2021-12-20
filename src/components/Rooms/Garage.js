b78717846ea4022568bf5c4da59a3069b3d40885
jayouimet <jayouimet@hotmail.com>
2021-12-19 20:25:06 -0500

    nice garage

--- src/components/Rooms/Garage.js
+++ src/components/Rooms/Garage.js
@@ -1,6 +1,10 @@
 import React from 'react';
+import BlackRubberMat from '../assets/BlackRubberMat';
 import Door from '../assets/Door';
+import GarageShelf from '../assets/GarageShelf';
+import GarbageBin from '../assets/GarbageBin';
 import Wall from '../assets/Wall';
+import ShelfOfPaint from '../objects/ShelfOfPaint';
 
 const Garage = (props) => {
     return (
@@ -10,6 +14,14 @@ const Garage = (props) => {
                 <Wall width="13.5" depth="0.2" height="4" position="0.25 0 -9" />
                 <Door position={"0.25 0 5"} wallWidth={13.5} wallHeight={4} doorHeight={3} doorWidth={2} depth="0.2" />
                 <Door position={"7 0 -2"} wallWidth={14} wallHeight={4} doorHeight={3} doorWidth={2} depth="0.2" rotation="0 90 0"/>
+
+                <a-entity position="0 -2 0">
+                    <GarageShelf position="-3.5 0 -8.2"/>
+                    <ShelfOfPaint position="0.5 0 -8.2"/>
+                    <ShelfOfPaint position="4.5 0 -8.2"/>
+                    <GarbageBin position="6 0 4.2"/>
+                    <BlackRubberMat position="5.8 0 -2" rotation="0 90 0"/>
+                </a-entity>
             </a-entity>
         </a-entity>
     );
