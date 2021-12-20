d0627784bead27c367f5d5c0f7c5d9e3520d7e87
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2021-12-19 20:23:25 -0500

    :) AFRAME

--- src/App.js
+++ src/App.js
@@ -3,41 +3,19 @@ import Camera from './components/assets/Camera';
 import ShelfOfPaint from './components/objects/ShelfOfPaint';
 import WorkDesk from './components/objects/WorkDesk';
 import Home from './components/objects/Home';
-import DoorTexture from "./textures/door.png";
-import Door1Texture from "./textures/door1.png";
-import FloorTexture from "./textures/floor.jpg";
-import WallTexture from "./textures/wall.jpg";
-import CeilingTexture from "./textures/ceiling.jpg";
-import RoofTexture from "./textures/roof.png";
-import BlindsTexture from "./textures/blinds.jpg";
 import GarbageBin from './components/assets/GarbageBin';
 import Cactus from './components/assets/Cactus';
 import Freezer from './components/assets/Freezer';
 import Oven from './components/assets/Oven';
 import KitchenSink from './components/assets/KitchenSink';
-import counterWood from './textures/counterWood.jpg';
-import kitchenTableWood from './textures/kitchenTableWood.jpg';
 import KitchenTable from './components/assets/KitchenTable';
 import KitchenChair from './components/assets/KitchenChair';
 import BlackRubberMat from './components/assets/BlackRubberMat';
-import blackRubberMat from './textures/blackRubberMat.jpg';
 
 function App() {
   return (
-    <a-scene defaultLightning="false">
-      <a-assets>
-        <img src={DoorTexture} id="door" alt="door"/>
-        <img src={Door1Texture} id="door1" alt="door1"/>
-        <img width="1" height="1" src={FloorTexture} id="floor" alt="floor"/>
-        <img width="1" height="1" src={WallTexture} id="wall" alt="wall"/>
-        <img width="1" height="1" src={CeilingTexture} id="ceiling" alt="ceiling"/>
-        <img width="1" height="1" src={RoofTexture} id="roof" alt="roof"/>
-        <img width="1" height="1" src={BlindsTexture} id="blinds" alt="blinds"/>
-        <img src={counterWood} id="counterWood" alt="counterWood"/>
-        <img src={kitchenTableWood} id="kitchenTableWood" alt="kitchenTableWood"/>
-        <img src={blackRubberMat} id="blackRubberMat" alt="blackRubberMat"/>
-      </a-assets>
-      
+    <a-entity>
+      <a-entity gltf-model="#bed" scale="0.75 0.75 0.75"></a-entity>
       <a-entity position={"0 0 0"} scale={"2 1 2"} environment="
         playArea: 1;
         preset: forest; 
@@ -48,19 +26,19 @@ function App() {
         lightPosition:0 15 0;
         flatShading:true;
         "></a-entity>
-      <Camera/>
-      <ShelfOfPaint position={"0 0 -12"}/>
-      <WorkDesk position={"0 0 -10"}/>
-      <GarbageBin position={"0 0 -8"}/>
-      <Cactus position={"0 0 -6"}/>
-      <Freezer position={"0 0 -4"}/>
-      <Oven position={"0 0 -2"}/>
-      <KitchenSink position={"2 0 0"}/>
-      <KitchenTable position={"2 0 -6"}/>
-      <KitchenChair position={"2 0 -4"}/>
-      <BlackRubberMat position={"2 0 -2"}/>
-      <Home position={"0 2 0"}/>
-    </a-scene>
+      <Camera />
+      <ShelfOfPaint position={"0 0 -12"} />
+      <WorkDesk position={"0 0 -10"} />
+      <GarbageBin position={"0 0 -8"} />
+      <Cactus position={"0 0 -6"} />
+      <Freezer position={"0 0 -4"} />
+      <Oven position={"0 0 -2"} />
+      <KitchenSink position={"2 0 0"} />
+      <KitchenTable position={"2 0 -6"} />
+      <KitchenChair position={"2 0 -4"} />
+      <BlackRubberMat position={"2 0 -2"} />
+      <Home position={"0 2 0"} />
+    </a-entity>
   );
 }
 
