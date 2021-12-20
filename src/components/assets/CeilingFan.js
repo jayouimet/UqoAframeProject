34275b243a09c435b36674866f2764cc95cced60
Francis Painchaud <44142080+Darkxys@users.noreply.github.com>
2021-12-20 17:36:43 -0500

    Ceiling fan animation onClick implemented

--- src/components/assets/CeilingFan.js
+++ src/components/assets/CeilingFan.js
@@ -1,9 +1,27 @@
 import React from 'react';
 
+if (!window.AFRAME.components["fan-component"]) {
+    window.AFRAME.registerComponent('fan-component', {
+        schema: { duration: { default: 0.5 } },
+        init: function () {
+            this.isFanOn = true;
+            this.element = this.el.querySelector(".beyblade");
+            this.el.addEventListener('click', this.OnFanClick.bind(this));
+        },
+        OnFanClick: function (event) {
+            this.isFanOn = !this.isFanOn;
+            if (this.isFanOn)
+                this.element.emit(":)");
+            else
+                this.element.emit(":(");
+        }
+    });
+}
+
 const CeilingFan = (props) => {
-    return(
+    return (
         <a-entity {...props}>
-            <a-entity>
+            <a-entity fan-component>
                 <a-cylinder color="#AAAAAA" height="0.1" radius="0.2"></a-cylinder>
                 <a-cone position="0 -0.075 0" height="0.05" radius-bottom="0.15" radius-top="0.2" color="#AAAAAA"></a-cone>
                 <a-cylinder position="0 -0.1 0" color="#AAAAAA" height="0.1" radius="0.05"></a-cylinder>
@@ -14,34 +32,36 @@ const CeilingFan = (props) => {
                     <a-cone position="0 -0.075 0" height="0.025" radius-bottom="0.19" radius-top="0.23" color="#AAAAAA"></a-cone>
                     <a-cone position="0 -0.1 0" height="0.025" radius-bottom="0.12" radius-top="0.19" color="#AAAAAA"></a-cone>
 
-                    <a-entity  rotation="0 0 0">
-                        <a-entity position="0 0 0.3">
-                            <a-box height="0.01" width="0.025" depth="0.3"></a-box>
-                            <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                    <a-entity class="beyblade" animation="autoplay:true;property:rotation;loop:true;dur:1000;to: 0 360 0;easing:linear;pauseEvents: :(;resumeEvents: :);">
+                        <a-entity rotation="0 0 0">
+                            <a-entity position="0 0 0.3">
+                                <a-box height="0.01" width="0.025" depth="0.3"></a-box>
+                                <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                            </a-entity>
                         </a-entity>
-                    </a-entity>
-                    <a-entity rotation="0 72 0">
-                        <a-entity position="0 0 0.3">
-                            <a-box height="0.01" width="0.025" depth="0.3"></a-box>
-                            <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                        <a-entity rotation="0 72 0">
+                            <a-entity position="0 0 0.3">
+                                <a-box height="0.01" width="0.025" depth="0.3"></a-box>
+                                <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                            </a-entity>
                         </a-entity>
-                    </a-entity>
-                    <a-entity rotation="0 144 0">
-                        <a-entity position="0 0 0.3">
-                            <a-box height="0.01" width="0.025" depth="0.3"></a-box>
-                            <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                        <a-entity rotation="0 144 0">
+                            <a-entity position="0 0 0.3">
+                                <a-box height="0.01" width="0.025" depth="0.3"></a-box>
+                                <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                            </a-entity>
                         </a-entity>
-                    </a-entity>
-                    <a-entity rotation="0 216 0">
-                        <a-entity position="0 0 0.3">
-                            <a-box height="0.01" width="0.025" depth="0.3"></a-box>
-                            <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                        <a-entity rotation="0 216 0">
+                            <a-entity position="0 0 0.3">
+                                <a-box height="0.01" width="0.025" depth="0.3"></a-box>
+                                <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                            </a-entity>
                         </a-entity>
-                    </a-entity>
-                    <a-entity rotation="0 288 0">
-                        <a-entity position="0 0 0.3">
-                            <a-box height="0.01" width="0.025" depth="0.3"></a-box>
-                            <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                        <a-entity rotation="0 288 0">
+                            <a-entity position="0 0 0.3">
+                                <a-box height="0.01" width="0.025" depth="0.3"></a-box>
+                                <a-box src="#counterWood" rotation="0 0 8" position="0 0 0.425" height="0.015" width="0.2" depth="0.75"></a-box>
+                            </a-entity>
                         </a-entity>
                     </a-entity>
                 </a-entity>
