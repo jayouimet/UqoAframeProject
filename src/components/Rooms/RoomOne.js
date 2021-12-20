492fd51a949b585b225badd037669cabc52466d0
jayouimet <jayouimet@hotmail.com>
2021-12-20 18:02:47 -0500

    Fixed a window and loading text

--- src/components/Rooms/RoomOne.js
+++ src/components/Rooms/RoomOne.js
@@ -11,7 +11,7 @@ const RoomOne = (props) => {
             <a-entity position="-15 0 -12.5">
                 <Wall width="15" depth="0.2" height="4" position="5 0 0" rotation="0 90 0" />
                 <Door position={"0 0 7.5"} wallWidth={10} wallHeight={4} doorHeight={3} doorWidth={2} depth="0.2" />
-                <Window position={"0 0 -7.5"} wallWidth={10} wallHeight={4} windowHeight={1.6} windowWidth={3} depth="0.2" />
+                <Window rotation="0 180 0" position={"0 0 -7.5"} wallWidth={10} wallHeight={4} windowHeight={1.6} windowWidth={3} depth="0.2" />
 
                 <a-entity position={"0 -2 0"}>
                     <Bed position={"-3.5 0 -5.2"} rotation={"0 180 0"} scale={"0.8 0.8 0.8"}/>
